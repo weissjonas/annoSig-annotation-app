90849ac6493b8d658015cdd19ff5b4d42c26ea50
jordan.mcewen <jordan.mcewen@ucalgary.ca>
2019-06-13 11:43:28 +0200

    Import Kivy Widgets

---
+++ SwipeApp.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+
+from kivy.uix.scatterlayout import ScatterLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.carousel import Carousel
+from kivy.uix.button import Button
+
+
+class SwipeApp(App):
+    pass
+
+if __name__=="__main__":
+    SwipeApp().run()
