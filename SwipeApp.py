8b43204ed499c21947575153be41b91f72b0f72d
jordan.mcewen <jordan.mcewen@ucalgary.ca>
2019-06-19 16:07:44 +0200

    Replaces image on click

--- SwipeApp.py
+++ SwipeApp.py
@@ -1,25 +1,45 @@
+import kivy
 from kivy.app import App
 
-from kivy.uix.scatterlayout import ScatterLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.widget import Widget
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.carousel import Carousel
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.properties import NumericProperty, ListProperty, BooleanProperty
 from kivy.uix.button import Button
 
+from glob import glob
+from os.path import join,dirname
+
+image_list = []
+
+class CustImage(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super(CustImage, self).__init__(**kwargs)
 
 class LayoutWidget(BoxLayout):
     def __init__(self, **kwargs):
         super(LayoutWidget, self).__init__(**kwargs)
 
+        for filename in glob("*.png"):
+            im = str(filename)
+            image_list.append(im)
+
+    def replace_image(self, widget):
+
+        self.ids.boxlayout.remove_widget(widget)
+        self.ids.boxlayout.add_widget(CustImage(source='test2.png'))
+
+
+
+
+
+
+
+
 
 class SwipeApp(App):
     def build(self):
         return LayoutWidget()
-
 
 if __name__=="__main__":
     SwipeApp().run()
