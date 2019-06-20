9a496ac4f6469df4f7a467b11be4eaaa2bb4b811
jordan.mcewen <jordan.mcewen@ucalgary.ca>
2019-06-20 13:05:36 +0200

    Stores added widget id in dictionary

--- SwipeApp.py
+++ SwipeApp.py
@@ -4,19 +4,23 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.behaviors import ButtonBehavior
-from kivy.properties import NumericProperty, ListProperty, BooleanProperty
+from kivy.uix.label import Label
+from kivy.properties import NumericProperty, ListProperty, BooleanProperty, DictProperty
 from kivy.uix.button import Button
 
 from glob import glob
-from os.path import join,dirname
+
 
 image_list = []
+count = 0
 
 class CustImage(ButtonBehavior, Image):
     def __init__(self, **kwargs):
         super(CustImage, self).__init__(**kwargs)
 
 class LayoutWidget(BoxLayout):
+    dynamic_ids = DictProperty({})
+
     def __init__(self, **kwargs):
         super(LayoutWidget, self).__init__(**kwargs)
 
@@ -25,16 +29,24 @@ class LayoutWidget(BoxLayout):
             image_list.append(im)
 
     def replace_image(self, widget):
+        global count
+        if count < len(image_list):
+            print(self.ids)
 
-        self.ids.boxlayout.remove_widget(widget)
-        self.ids.boxlayout.add_widget(CustImage(source='test2.png'))
+            self.ids.boxlayout.remove_widget(widget)
+            id = 'image'+str(count)
+            im = CustImage(source = str(image_list[count]), id = id)
 
+            self.ids.boxlayout.add_widget(im)
 
+            print(self.dynamic_ids)
 
+            count += 1
 
+            return im
 
-
-
+    def callback(self, value):
+        pass
 
 
 class SwipeApp(App):
