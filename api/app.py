6ba46a7a7bd3d51a9badc3a9e1cad7587119a897
Jonas Weiß <jonas.weiss@tu-dresden.de>
2019-09-23 22:09:28 +0200

    Adds simple API access token

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 import json
-from flask import Flask, request
+from flask import Flask, request, abort
 
+
+API_TOKEN = "0e2d70b1f642f85550adb7ff20656462"
 
 app = Flask(__name__)
 
@@ -13,11 +15,15 @@ def hello_world():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    token = request.headers.get("Authorization")
+    if token != API_TOKEN:
+        abort(401)
+
     data = request.get_json()
     filename = f"data-{datetime.now().timestamp()}.json"
 
     # TODO: Validation before writing to file
-    with open(filename, 'wb') as f:
+    with open(filename, 'w') as f:
         json.dump(data, f, indent=2)
 
     return ''
